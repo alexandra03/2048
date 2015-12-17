9d74394f7775c16d3ae0391e84f035eddf806e6a
= <ryan.catoen@gmail.com>
2015-12-17 17:20:02 -0500

    Added conditition to only reduce search depth up to a certain score

--- js/ai.js
+++ js/ai.js
@@ -7,7 +7,7 @@ function AI(game) {
 	this.LNGWeight = 1;
 	this.CVWeight  = 0.1;
 
-	this.maxSearchDepth = 3;
+	this.maxSearchDepth = 1;
 
 	this.checkThreshold = 6;
 }
@@ -201,7 +201,7 @@ AI.prototype.lookAhead = function () {
 };
 
 AI.prototype.searchLookAheadHelper = function (depth){
-	if (depth == this.maxSearchDepth){
+	if (depth == this.maxSearchDepth || (this.game.grid.availableCells() > 8 && this.score < 2000 && depth == 2)){
 		return [0, this.lookAhead()];
 	}
 
